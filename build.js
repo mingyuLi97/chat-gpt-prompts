b68c9291ec6f83196d969c31816bf612400c8baf
limy <mingyuli1997@gmail.com>
2023-04-12 22:53:46 +0800

    build: 修改打包文件；

--- build.js
+++ build.js
@@ -2,45 +2,32 @@
 var crx3 = require('crx3'),
   path = require('path'),
   fs = require('fs'),
-  packageJson = require('./package.json'),
-  manifestJson = require('./src/manifest.json');
+  packageJson = require('./package.json');
 
-const context = process.cwd();
 let manifestPath = path.join('./dist', 'manifest.json');
 const name = packageJson.name;
 
-updateVersion();
 console.log(`Building '${name}' with manifest from ${manifestPath}`);
 
 try {
-  const extensionPath = path.join('dist', name + '.zip');
-  if (fs.existsSync(extensionPath)) {
-    fs.rmSync(extensionPath);
+  const crxPath = path.join('dist', name + '.crx');
+  const zipPath = path.join('dist', name + '.zip');
+  if (fs.existsSync(crxPath)) {
+    fs.rmSync(crxPath);
+  }
+  if (fs.existsSync(zipPath)) {
+    fs.rmSync(zipPath);
   }
   crx3([manifestPath], {
     keyPath: path.join('secrets', 'private-key.pem'),
-    zipPath: extensionPath,
+    crxPath,
+    zipPath,
   }).then(function () {
-    console.log('Extension package written to ' + extensionPath);
+    console.log(
+      'Extension package written to ' + path.join('dist', name + '.crx')
+    );
   });
 } catch (errorMsg) {
   console.error(`Error: ${errorMsg}s`);
   return 1;
-}
-
-function updateVersion() {
-  const version = packageJson.version;
-  const idx = version.lastIndexOf('.') + 1;
-  const newVersion =
-    version.substring(0, idx) + (parseInt(version.substring(idx)) + 1);
-  packageJson.version = newVersion;
-  manifestJson.version = newVersion;
-  fs.writeFileSync(
-    path.resolve(context, 'package.json'),
-    JSON.stringify(packageJson, null, 2) + '\n'
-  );
-  fs.writeFileSync(
-    path.resolve(context, 'src/manifest.json'),
-    JSON.stringify(manifestJson, null, 2) + '\n'
-  );
 }
